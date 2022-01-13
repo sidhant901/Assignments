7af1c8b0258071a40569648175c2f3b484534356
sidhant901 <97655782+sidhant901@users.noreply.github.com>
2022-01-13 00:37:55 -0500

    Add files via upload

---
+++ anudip1.java
@@ -0,0 +1,7 @@
+class anudip1
+{
+  public static void main(String [] args)
+  {
+    System.out.println("i am sidhant prakash jadhav ,currently i have completed b.tech in computer science ");
+	}
+	}
