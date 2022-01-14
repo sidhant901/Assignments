aa95b293afac819c43dcb896c87169514fae24f2
sidhant901 <97655782+sidhant901@users.noreply.github.com>
2022-01-14 11:41:51 +0530

    Update anudip1.java

--- anudip1.java
+++ anudip1.java
@@ -5,7 +5,7 @@ class anudip1
     System.out.println("i am sidhant prakash jadhav ,currently i have completed b.tech in computer science ");
 	}
 }
-/
+/*
 D:\AnudipJavaPrograms>java anudip1
 i am sidhant prakash jadhav ,currently i have completed b.tech in computer science
-/
+*/
