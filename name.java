231c9b1dcc69998d4c9f697ee02678ca97b2c482
sidhant901 <97655782+sidhant901@users.noreply.github.com>
2022-01-14 11:28:35 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class name
+{
+  public static void main(String[] args)
+  {
+    String s;
+    double p;
+   Scanner sc=new Scanner(System.in);
+    System.out.println("Enter your name");
+    s=sc.nextLine();
+    System.out.println("Enter your percentage");
+    p=sc.nextDouble();
+     System.out.println("Your name is = "+s);
+      System.out.println("Your percentage is="+p);
+    }
+}
+
+
+output -
+
+D:\AnudipJavaPrograms>java name
+Enter your name
+Sidhant Prakash Jadhav
+Enter your percentage
+61.05
+Your name is = Sidhant Prakash Jadhav
+Your percentage is=61.05
+    
